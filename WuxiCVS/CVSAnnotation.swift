7a7e1fec979a43785f653df47314f35985a36ec3
Chun Cao <chun@ChundeMacBook.local>
2016-11-24 23:42:34 +0800

    updates

--- WuxiCVS/CVSAnnotation.swift
+++ WuxiCVS/CVSAnnotation.swift
@@ -14,11 +14,11 @@ class CVSAnnotation: NSObject, MKAnnotation {
     
     var title: String?
     var coordinate: CLLocationCoordinate2D
-    var info: String
+    var subtitle: String?
     
     init(title: String, coordinate: CLLocationCoordinate2D, info: String) {
         self.title = title
         self.coordinate = coordinate
-        self.info = info
+        self.subtitle = info
     }
 }
--- WuxiCVS/CVSMapViewController.swift
+++ WuxiCVS/CVSMapViewController.swift
@@ -94,6 +94,26 @@ class CVSMapViewController: UIViewController, MKMapViewDelegate {
         
     }
     
+//    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+//        if let annotation = annotation as? CVSAnnotation {
+//            let identifier = "pin"
+//            var view: MKPinAnnotationView
+//            if let dequeuedView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+//                as? MKPinAnnotationView {
+//                dequeuedView.annotation = annotation
+//                view = dequeuedView
+//            } else {
+//                // 3
+//                view = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+//                view.canShowCallout = true
+//                view.calloutOffset = CGPoint(x: -5, y: 5)
+//                view.rightCalloutAccessoryView = UIButton(type: .detailDisclosure) as UIView
+//            }
+//            return view
+//        }
+//        return nil
+//
+//    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
