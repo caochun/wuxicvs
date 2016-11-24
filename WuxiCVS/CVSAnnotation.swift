163ca817a6d84a3a75d4d249bf959027dbc3c786
Chun Cao <chun@ChundeMacBook.local>
2016-11-24 23:12:37 +0800

    15 points

---
+++ WuxiCVS/CVSAnnotation.swift
@@ -0,0 +1,24 @@
+//
+//  CVSAnnotation.swift
+//  WuxiCVS
+//
+//  Created by Chun Cao on 2016/11/23.
+//  Copyright © 2016年 Nemoworks. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+
+class CVSAnnotation: NSObject, MKAnnotation {
+    
+    var title: String?
+    var coordinate: CLLocationCoordinate2D
+    var info: String
+    
+    init(title: String, coordinate: CLLocationCoordinate2D, info: String) {
+        self.title = title
+        self.coordinate = coordinate
+        self.info = info
+    }
+}
---
+++ WuxiCVS/CVSMapViewController.swift
@@ -0,0 +1,113 @@
+//
+//  CVSMapViewController.swift
+//  WuxiCVS
+//
+//  Created by Chun Cao on 2016/11/23.
+//  Copyright © 2016年 Nemoworks. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class CVSMapViewController: UIViewController, MKMapViewDelegate {
+    
+    var json: JSON = JSON.null
+
+    @IBOutlet weak var cvsMapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        cvsMapView.delegate = self
+        
+        var cvsList = [CVSAnnotation]()
+        
+        if let file = Bundle.main.path(forResource: "cvs", ofType: "json") {
+            do {
+                let data = try Data(contentsOf: URL(fileURLWithPath: file))
+                let json = JSON(data: data)
+                self.json = json
+                
+                if json.type == .array {
+                    let cvss = json.arrayValue
+                    for cvs in cvss{
+                        let name = cvs["name"].stringValue
+                        let addr = cvs["address"].stringValue
+                        let lati = cvs["latitude"].stringValue
+                        let longitude = cvs["longitude"].stringValue
+                        
+                        
+                        cvsList.append(CVSAnnotation(title: name, coordinate: CLLocationCoordinate2D(latitude: Double(lati)!,longitude:Double(longitude)!), info: addr))
+                    }
+                    
+                }
+            } catch {
+                self.json = JSON.null
+            }
+        } else {
+            self.json = JSON.null
+        }
+    
+        
+        cvsMapView.addAnnotations(cvsList)
+        
+        let visibleMap = findApporiateRegion(annotations: cvsList)
+        
+        cvsMapView.setRegion(MKCoordinateRegion(center: visibleMap.0, span: visibleMap.1), animated: true)
+    }
+    
+    
+    
+    func findApporiateRegion(annotations: [CVSAnnotation]) ->  (CLLocationCoordinate2D,MKCoordinateSpan){
+        
+        var maxLat: CLLocationDegrees = 0.0
+        var minLat: CLLocationDegrees = 360
+        var maxLon: CLLocationDegrees = 0.0
+        var minLon: CLLocationDegrees = 360
+        
+        for annotation in annotations{
+            let location = annotation.coordinate
+            
+            if location.latitude > maxLat{
+                maxLat = location.latitude
+            }
+            
+            if location.latitude < minLat{
+                minLat = location.latitude
+            }
+            
+            if location.longitude > maxLon {
+                maxLon = location.longitude
+            }
+            
+            if location.longitude < minLon {
+                minLon = location.longitude
+            }
+        }
+        
+        let center  = CLLocationCoordinate2D(latitude: (maxLat + minLat)/2, longitude: (maxLon + minLon)/2)
+        
+        let span = MKCoordinateSpan(latitudeDelta: (maxLat - minLat)*1.2, longitudeDelta: (maxLon - minLon)*1.2)
+        
+        return (center, span)
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
